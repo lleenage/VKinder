afb69b7c48109d91ca011bcf6851d4451b4bd7f9
Poleen <fxcklleewyb@gmail.com>
2025-02-05 21:09:00 +0300

    some methot added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# python -m pip install vk_api
-# python -m pip install bs4
+#pip install vk_api
+#pip install requests
 from random import randrange
 
 import vk_api
@@ -12,12 +12,12 @@ from vkbot import VkBot
 # token = input('Token: ')
 token = token
 
-vk = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk)
+vk_session = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(vk_session)
 
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+    vk_session.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
 
 
 print("Бот запущен")
@@ -26,7 +26,7 @@ for event in longpoll.listen():
         if event.to_me:
             print(f'Новое сообщение от пользователя с id {event.user_id}: ', end='')
 
-            bot = VkBot(event.user_id, token)
+            bot = VkBot(token, event.user_id)
             write_msg(event.user_id, bot.new_message(event.text))
 
             print(f'"{event.text}"')
--- vkbot.py
+++ vkbot.py
@@ -1,23 +1,102 @@
 import requests
 import json
+from datetime import date
+
 
 class VkBot:
-    def __init__(self, user_id, token, version="5.131"):
+    def __init__(self, vk_token, user_id, version="5.131"):
         self.user_id = user_id
-        self.username = self.get_user_name(user_id)
-        self.token = token
+        self.base_url = 'https://api.vk.com/method/'
+        self.token = vk_token
         self.version = version
-        self.base_url = "https://api.vk.com/method/"
-        self.params = {"access_token": self.token, "v": self.version}
-        self.commands = ["ПРИВЕТ", "ПОКА"]
+        self.params = {'access_token': self.token, 'v': self.version}
 
-    #метод для получения возраста, пола, города пользователя
-    def get_info(self):
-        pass
+        self.commands = ["ПРИВЕТ", "ПОКА", "МОЙ ГОРОД", "МОЙ ПОЛ"]
 
-    #метод получения имени пользователя
-    def get_user_name(self, user_id):
-        pass
+    #методы для получения родного города пользователя и города, в котором живет пользователь
+    def get_home_town(self):
+        url = self.base_url + 'users.get?'
+        param = {
+            **self.params,
+            'user_ids': self.user_id,
+            'fields': 'home_town'
+        }
+        response = requests.get(url, params=param)
+        try:
+            users_town = response.json()['response'][0]['home_town']
+            return users_town
+        except:
+            return 'Родной город не указан'
+
+    def get_city(self):
+        url = self.base_url + 'users.get?'
+        param = {
+            **self.params,
+            'user_ids': self.user_id,
+            'fields': 'city'
+        }
+        response = requests.get(url, params=param)
+        try:
+            users_city = response.json()['response'][0]['title']
+            return users_city
+        except:
+            return 'Город не указан'
+
+    #метод для получения пола
+    def get_sex(self):
+        url = self.base_url + 'users.get?'
+        param = {
+            **self.params,
+            'user_ids': self.user_id,
+            'fields': 'sex'
+        }
+        response = requests.get(url, params=param)
+        sex = response.json()['response'][0]['sex']
+        if sex == 1:
+            user_gender = 'Женский пол'
+        elif sex == 2:
+            user_gender = 'Мужской пол'
+        else:
+            user_gender = 'Пол не указан'
+        return user_gender
+
+
+    #метод для получения имени и фамилии(
+    # get_name()[0] - имя
+    # get_name()[1] - фамилия
+    # )
+    def get_name(self):
+        url = self.base_url + 'users.get?'
+        param = {
+            **self.params,
+            'user_ids': self.user_id,
+            'fields': 'about'
+
+        }
+        response = requests.get(url, params=param)
+        self.user_first_name = response.json()['response'][0]['first_name']
+        self.user_last_name = response.json()['response'][0]['last_name']
+
+        return self.user_first_name, self.user_last_name
+
+    #метод получения возраста(нужно доделать, либо я плохо искала и не нашла нужный метод для получения возраста)
+    def get_age(self):
+        url = self.base_url + 'users.get?'
+        param = {
+            **self.params,
+            'user_ids': self.user_id,
+            'fields': 'bdate'
+        }
+        response = requests.get(url, params=param)
+        try:
+            birth_date = response.json()['response'][0]['bdate']
+            time_now = date.today().strftime('%d.%m.%Y')
+
+            #тут будет код для вычесления возраста
+
+            return birth_date, time_now
+        except:
+            return 'Год или дата рождения не указана'
 
     #метод получения 3 фотографий с наибольшим количеством лайков
     def get_photo(self, user_id, count=3):
@@ -42,10 +121,16 @@ class VkBot:
     #метод по обработке тех или иных сообщений
     def new_message(self, message):
         if message.upper() == self.commands[0]:
-            return f"Привет, {self.username}!"
+            return f"Привет, {self.get_name()[0]}!"
 
+        elif message.upper() == self.commands[1]:
+            return f"До встречи, {self.get_name()[0]}!"
+        #проверяю на работоспособность получение города пользователя
+        elif message.upper() == self.commands[2]:
+            town = [self.get_home_town(),  self.get_city()]
+            return town
+        #проверка медотода получения гендера
         elif message.upper() == self.commands[3]:
-            return f"До встречи, {self.username}!"
-
+            return self.get_sex()
         else:
             return "Я пока не знаю такой команды. 😞"
