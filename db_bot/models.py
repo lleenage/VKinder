e7cb72c016a0f13984ff4e9de491541782f469d2
Sergey Karyukhin <prajs601@yandex.ru>
2025-02-10 22:26:52 +0300

    Changed connection_db.py and Added models.py

---
+++ db_bot/models.py
@@ -0,0 +1,60 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    vk_id = Column(Integer, unique=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    age = Column(Integer)
+    gender = Column(String(1))  # M/F
+    city = Column(String(50))
+
+    def __repr__(self):
+        return f"<User(vk_id={self.vk_id}, first_name='{self.first_name}', last_name='{self.last_name}')>"
+
+class ProfilePhoto(Base):
+    __tablename__ = 'profile_photo'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    photo_url = Column(String, nullable=False)
+
+    user = relationship("User")
+
+    def __repr__(self):
+        return f"<ProfilePhoto(user_id={self.user_id}, photo_url='{self.photo_url}')>"
+
+class FavoriteUser(Base):
+    __tablename__ = 'favorite_user'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    favorited_by = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, server_default=func.now())
+
+    user = relationship("User", foreign_keys=[user_id])
+    favoriter = relationship("User", foreign_keys=[favorited_by])
+
+    def __repr__(self):
+        return f"<FavoriteUser(user_id={self.user_id}, favorited_by={self.favorited_by}, created_at={self.created_at})>"
+
+class Blacklist(Base):
+    __tablename__ = 'blacklist'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    blocked_by = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, server_default=func.now())
+
+    blocked_user = relationship("User", foreign_keys=[user_id])
+    blocker = relationship("User", foreign_keys=[blocked_by])
+
+    def __repr__(self):
+        return f"<Blacklist(blocked_user_id={self.blocked_user_id}, blocked_by={self.blocked_by}, created_at={self.created_at})>"
